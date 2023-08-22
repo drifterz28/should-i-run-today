caafd689289fe8d9059f8f97ff297756a8a4b4c5
Chris Whitney <drifterz28@users.noreply.github.com>
2023-08-22 16:52:23 +0000

    Add TS

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,9 +2,24 @@ import { useEffect, useState } from "react";
 import { shouldIRunYes, shouldIRunNo } from "./constants";
 import { getRandomInt } from "./helpers";
 
+export type weatherData = {
+  aqiIndex: number;
+  feels_like: number;
+  humidity: number;
+  speed: number;
+};
+
+const defaultWeatherData = {
+  aqiIndex: 0,
+  feels_like: 0,
+  humidity: 0,
+  speed: 0,
+};
+
 export const useShouldIRunText = () => {
   const [text, setText] = useState("");
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] =
+    useState<weatherData>(defaultWeatherData);
   const { aqiIndex, feels_like, humidity, speed } = weatherData;
   const isGood = aqiIndex < 3 && feels_like < 90 && humidity < 90 && speed < 20;
 
